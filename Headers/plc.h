7247b6ee4244bb206b7b4f4dab26bf3c5fe1d27d
Albert Muller <jzxdrift@users.noreply.github.com>
2024-08-23 22:40:43 -0700

    Update and rename plc.h to Headers/plc.h

--- Headers/plc.h
+++ Headers/plc.h
@@ -5,11 +5,11 @@
 #include "pid.h"
 
 #define MAX_SAFE_TEMP			(80)		/* maximum safe operating temperature in Celsius */
-#define IGNITION_ON				(1)			/* ignition switch is on */
-#define IGNITION_OFF			(0)			/* ignition switch is off */
+#define IGNITION_ON			(1)		/* ignition switch is on */
+#define IGNITION_OFF			(0)		/* ignition switch is off */
 #define TEMP_SENSOR_ERROR		(-1)		/* error code for sensor malfunction */
 
-#define MIN_DUTY_CYCLE			(0)			/* minimum pump/fan PWM duty cycle */
+#define MIN_DUTY_CYCLE			(0)		/* minimum pump/fan PWM duty cycle */
 #define MAX_DUTY_CYCLE			(100)		/* maximum pump/fan PWM duty cycle */
 
 typedef enum
